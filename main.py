fd419fa507433e461f4ec544ae26d7bfb30fcccc
william.af <william.af@elitestek.com>
2024-07-10 11:41:52 +0800

    uncommented main

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ api_key="AIzaSyC-auhr_7A-I8LBsl58X3YzH7y5K7_YAoE"
 permalink_csv = os.path.join(save_folder, "permalist.csv")
 
 if __name__ == "__main__":
-    # # step 1
-    # permaObj = permalink(url, permalink_csv)
-    # permaObj.perma_csv()
+    # step 1
+    permaObj = permalink(url, permalink_csv)
+    permaObj.perma_csv()
 
-    # # step 2
-    # permalink_csv = permalink_csv
-    # content_csv = "content.csv"
+    # step 2
+    permalink_csv = permalink_csv
+    content_csv = "content.csv"
 
-    # contentObj = content(permalink_csv, save_folder)
-    # contentObj.content_process()
+    contentObj = content(permalink_csv, save_folder)
+    contentObj.content_process()
 
     # step 3
     article_df = os.path.join(save_folder,'content_csv.csv')
